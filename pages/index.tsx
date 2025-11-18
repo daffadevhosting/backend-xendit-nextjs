d0bd7cf567f3dbc7d502c304110c7042de95d4b7
Daffa Aditya <79897029+daffadevhosting@users.noreply.github.com>
2025-11-18 21:25:20 +0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// pages/index.tsx
+import PaymentTester from '../components/PaymentTester';
+
+export default function Home() {
+  return (
+    <div>
+      <PaymentTester />
+    </div>
+  );
+}
